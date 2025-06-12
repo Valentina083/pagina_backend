2303923892789b00fe07c98f7a09f195b961af8f
Valentina <valentinaortiz.083@gmail.com>
2025-06-11 21:22:44 -0500

    Se agregó variable en ALLOWED_HOSTS del settings.py

--- proyecto_backend/settings.py
+++ proyecto_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-y7hrvqwtbs8^wyk8k_uu^a)6ahw63abe9116-k#lgq&xtsl(+_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pagina-backend.onrender.com']
 
 
 # Application definition
